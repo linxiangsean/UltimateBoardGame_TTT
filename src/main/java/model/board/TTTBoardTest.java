b5c27a97767851b9cdbeb60a5a42fb22e63ddd65
linxiangsean <xxl180009@utdallas.edu>
2019-11-30 22:47:14 -0600

    Added test cases

--- src/main/java/model/board/TTTBoardTest.java
+++ src/main/java/model/board/TTTBoardTest.java
@@ -1,10 +1,35 @@
 package model.board;
 
 import model.GameModel;
+import model.board.TTTBoard;
 import org.testng.annotations.Test;
 
 import static org.junit.Assert.*;
 
 public class TTTBoardTest {
+
+    GameModel game = new GameModel(3, 3);
+
+    @Test
+    void moveTest(){
+        assertTrue(game.getBoardModel().isValidMove(1,0,1));
+        assertFalse(game.getBoardModel().isValidMove(1,3,3));
+    }
+
+    @Test
+    void resetTest(){
+
+        game.getBoardModel().reset();
+        int count = 0 ;
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 0; j < 3; j++){
+                if(game.getBoardModel().getCells()[i][j] == 0){
+                    count++;
+                }
+            }
+        }
+
+        assertEquals(9,count);
+    }
 
 }
